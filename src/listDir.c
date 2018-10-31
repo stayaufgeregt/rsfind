5c18bb10a424156daca17eed0eb9cb93ab88af4e
Ishara CHAN-TUNG <ishara.chan-tung@telecomnancy.eu>
2018-10-31 12:00:27 +0100

    Affichage correct gid et uid

--- src/listDir.c
+++ src/listDir.c
@@ -7,6 +7,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <grp.h>
+#include <pwd.h>
 
 void lsDir(char* path,args* mesArgs){
 
@@ -17,6 +19,14 @@ void lsDir(char* path,args* mesArgs){
 	//curent file being listed
 
 	struct stat path_stat;
+	//info about the file
+
+	struct group *info_gp;
+	//info about file's gid
+
+	struct passwd *info_usr;
+	//info about file's uid
+
 	dp=opendir(path);
 	
 	if(strcmp(path,".")==0){
@@ -44,7 +54,6 @@ void lsDir(char* path,args* mesArgs){
 
 								
 				if(mesArgs->flags[L]){
-					printf("HEYY\n");
 					stat(childPath,&path_stat);
 					(path_stat.st_mode & S_IFDIR)?(printf("d")):(printf("-"));
 					(path_stat.st_mode & S_IRUSR)?(printf("r")):(printf("-"));
@@ -57,8 +66,13 @@ void lsDir(char* path,args* mesArgs){
 					(path_stat.st_mode & S_IWOTH)?(printf("w")):(printf("-"));
 					(path_stat.st_mode & S_IXOTH)?(printf("x")):(printf("-"));
 					printf(" %d",(int)path_stat.st_nlink);
-					printf(" %d",(int)path_stat.st_uid);
-					printf(" %d",(int)path_stat.st_gid);
+
+					info_usr=getpwuid(path_stat.st_uid);
+					printf(" %s",info_usr->pw_name);
+
+					info_gp=getgrgid(path_stat.st_gid);
+					printf(" %s",info_gp->gr_name);
+
 					printf(" %ld",(unsigned long)path_stat.st_size);
 					char* tmp = ctime(&path_stat.st_ctime);
 					tmp[strlen(tmp)-1]='\0';
@@ -75,28 +89,7 @@ void lsDir(char* path,args* mesArgs){
 
 				} else {
 					// Affichage par defaut sans options
-					//printf("%s\n",childPath); A REMETTRE
-
-					stat(childPath,&path_stat);
-					(path_stat.st_mode & S_IFDIR)?(printf("d")):(printf("-"));
-					(path_stat.st_mode & S_IRUSR)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWUSR)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXUSR)?(printf("x")):(printf("-"));
-					(path_stat.st_mode & S_IRGRP)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWGRP)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXGRP)?(printf("x")):(printf("-"));
-					(path_stat.st_mode & S_IROTH)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWOTH)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXOTH)?(printf("x")):(printf("-"));
-					printf(" %d",(int)path_stat.st_nlink);
-					printf(" %d",(int)path_stat.st_uid);
-					printf(" %d",(int)path_stat.st_gid);
-					printf(" %ld",(unsigned long)path_stat.st_size);
-					char* tmp = ctime(&path_stat.st_ctime);
-					tmp[strlen(tmp)-1]='\0';
-					printf(" %s",tmp);
-					printf(" %s\n",ep->d_name);
-					// BLOC A ENLEVER QUAND TU AURAS VU
+					printf("%s\n",childPath);
 
 						if(ep->d_type==DT_DIR){
 							lsDir(childPath,mesArgs);
