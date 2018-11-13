5052c84028537f3fb1a8f9b093107ead57ec58f9
Ishara CHAN-TUNG <ishara.chan-tung@telecomnancy.eu>
2018-11-13 13:30:47 +0100

    restructuration

--- src/listDir.c
+++ src/listDir.c
@@ -11,6 +11,65 @@
 #include <pwd.h>
 #include <fcntl.h>
 
+char* findChild(char* path,struct dirent* ep){
+	char* childPath=malloc(sizeof(char)*(strlen(path)+1+strlen(ep->d_name)));
+		strcpy(childPath,path);
+		strcat(childPath,"/");
+		strcat(childPath,ep->d_name);
+		// on fabrique le chemin fils "pathPere/nomFichierFils"	
+	return childPath;
+
+}
+
+void affichageList(char* path){
+
+	struct stat path_stat;
+	//info about the file
+
+	struct group *info_gp;
+	//info about file's gid
+
+	struct passwd *info_usr;
+	//info about file's uid
+
+	stat(path,&path_stat);
+	(path_stat.st_mode & S_IFDIR)?(printf("d")):(printf("-"));
+	(path_stat.st_mode & S_IRUSR)?(printf("r")):(printf("-"));
+	(path_stat.st_mode & S_IWUSR)?(printf("w")):(printf("-"));
+	(path_stat.st_mode & S_IXUSR)?(printf("x")):(printf("-"));
+	(path_stat.st_mode & S_IRGRP)?(printf("r")):(printf("-"));
+	(path_stat.st_mode & S_IWGRP)?(printf("w")):(printf("-"));
+	(path_stat.st_mode & S_IXGRP)?(printf("x")):(printf("-"));
+	(path_stat.st_mode & S_IROTH)?(printf("r")):(printf("-"));
+	(path_stat.st_mode & S_IWOTH)?(printf("w")):(printf("-"));
+	(path_stat.st_mode & S_IXOTH)?(printf("x")):(printf("-"));
+	printf(" %d",(int)path_stat.st_nlink);
+
+	info_usr=getpwuid(path_stat.st_uid);
+	printf(" %s",info_usr->pw_name);
+
+	info_gp=getgrgid(path_stat.st_gid);
+	printf(" %s",info_gp->gr_name);
+
+	printf(" %ld",(unsigned long)path_stat.st_size);
+
+	char* tmp = ctime(&path_stat.st_ctime);
+	tmp = tmp + 4; // enlever le mois en toutes lettres
+	tmp[strlen(tmp)-9]='\0';// enlever les secs et l'année
+	tmp[0]=tmp[0]+32; // la maj du mos en min
+	char* tmp2 = malloc(strlen(tmp)*sizeof(char));
+	strcpy(tmp2,tmp);
+	tmp[3]='\0';
+	tmp2[1]='.';
+	tmp2[2]=' ';
+	tmp2 = tmp2 + 1;
+	strcat(tmp,tmp2);
+	
+	printf(" %s",tmp);
+	printf(" %s\n",path);
+	//free(tmp2);
+}
+
 void lsDir(char* path,args* myArgs){
 
 	DIR* dp;
@@ -22,11 +81,7 @@ void lsDir(char* path,args* myArgs){
 	struct stat path_stat;
 	//info about the file
 
-	struct group *info_gp;
-	//info about file's gid
-
-	struct passwd *info_usr;
-	//info about file's uid
+	static int start=1;
 
 	dp=opendir(path);
 	
@@ -45,52 +100,17 @@ void lsDir(char* path,args* myArgs){
 			//d_name == filename
 			
 			//if it is not current dir or parent dir
-			if(strcmp(ep->d_name,"..") && strcmp(ep->d_name,".") && strcmp(ep->d_name,".git")){
+			if(strcmp(ep->d_name,"..") && strcmp(ep->d_name,".")){
 
-				char childPath[strlen(path)+strlen(ep->d_name)+2];
-				strcpy(childPath,path);
-				strcat(childPath,"/");
-				strcat(childPath,ep->d_name);
-				// on fabrique le chemin fils "pathPere/nomFichierFils"
+				char *childPath=findChild(path,ep);
 
-								
 				if(myArgs->flags[L]){
-					stat(childPath,&path_stat);
-					(path_stat.st_mode & S_IFDIR)?(printf("d")):(printf("-"));
-					(path_stat.st_mode & S_IRUSR)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWUSR)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXUSR)?(printf("x")):(printf("-"));
-					(path_stat.st_mode & S_IRGRP)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWGRP)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXGRP)?(printf("x")):(printf("-"));
-					(path_stat.st_mode & S_IROTH)?(printf("r")):(printf("-"));
-					(path_stat.st_mode & S_IWOTH)?(printf("w")):(printf("-"));
-					(path_stat.st_mode & S_IXOTH)?(printf("x")):(printf("-"));
-					printf(" %d",(int)path_stat.st_nlink);
 
-					info_usr=getpwuid(path_stat.st_uid);
-					printf(" %s",info_usr->pw_name);
-
-					info_gp=getgrgid(path_stat.st_gid);
-					printf(" %s",info_gp->gr_name);
-
-					printf(" %ld",(unsigned long)path_stat.st_size);
-				//	struct tm *infoTime = gmtime(&path_stat.st_ctime);
-				//	printf(" %d. %d %d:%d",infoTime->tm_mon+1,infoTime->tm_mday+1,infoTime->tm_hour+1,infoTime->tm_min);
-					char* tmp = ctime(&path_stat.st_ctime);
-					tmp = tmp + 4; // enlever le mois en toutes lettres
-					tmp[strlen(tmp)-9]='\0';// enlever les secs et l'année
-					tmp[0]=tmp[0]+32; // la maj du mos en min
-					char* tmp2 = malloc(strlen(tmp)*sizeof(char));
-					strcpy(tmp2,tmp);
-					tmp[3]='\0';
-					tmp2[1]='.';
-					tmp2[2]=' ';
-					tmp2 = tmp2 + 1;
-					strcat(tmp,tmp2);
-					
-					printf(" %s",tmp);
-					printf(" %s\n",ep->d_name);
+					if(strcmp(path,myArgs->path)==0 && start){
+						start = 0;
+						affichageList(path);
+					}
+					affichageList(childPath);
 
 					if(ep->d_type==DT_DIR){
 						lsDir(childPath,myArgs);
@@ -132,7 +152,7 @@ void lsDir(char* path,args* myArgs){
 						}
 
 				}
-
+			//free(childPath);
 			}
 		}
 		closedir(dp);
