c5a3453a15fa629b5dbcf4340b5385c194a8358d
Ishara CHAN-TUNG <ishara.chan-tung@telecomnancy.eu>
2018-12-16 20:10:11 +0100

    commentaires en moins

--- src/text_matcher.c
+++ src/text_matcher.c
@@ -11,7 +11,7 @@
 bool regexPerl(char* fileToCmp,char* ptrn){
 
 	PCRE2_SPTR strCompare =(PCRE2_SPTR) fileToCmp;
-	PCRE2_SPTR pattern =(PCRE2_SPTR) ptrn; // remplacer "/nd/" par myArgs.T
+	PCRE2_SPTR pattern =(PCRE2_SPTR) ptrn; 
 
 	pcre2_code* regexPattern;
 	pcre2_match_data *match_data;
@@ -34,8 +34,8 @@ bool regexPerl(char* fileToCmp,char* ptrn){
 		// On regarde dans le fichier si quelque chose correspond au pattern
 		int a = pcre2_match(regexPattern,strCompare,strlen((char *)strCompare),0,0,match_data,NULL);
 
-	  	pcre2_match_data_free(match_data);   /* Release memory used for the match */
-	  	pcre2_code_free(regexPattern);                 /* data and the compiled pattern. */
+	  	pcre2_match_data_free(match_data);
+	  	pcre2_code_free(regexPattern);
 	  	
 		if(a>=1){
 			return 1;
@@ -49,9 +49,6 @@ bool regexPerl(char* fileToCmp,char* ptrn){
 
 
 bool regexNameMatch(char* fileName){
-	//printf("%s\n",name );
-	// printf("%s\n",myArgs.name );
-	// printf("%s\n",ename);
 	return fileName != NULL && regexPerl(fileName,myArgs.ename);
 }
 
@@ -68,53 +65,6 @@ bool regexTextMatch(char* path,struct stat* path_stat){
 	close(fd);
 	return regexPerl(fileBuffer,myArgs.T);
 }
-
-// bool regexTextMatch(char* path,struct stat* path_stat){
-
-// 	if(!S_ISREG(path_stat->st_mode))
-// 		return false;
-	
-// 	int fd;
-// 	fd = open(path,O_RDONLY);
-// 	char fileBuffer[path_stat->st_size];
-// 	read(fd,fileBuffer,path_stat->st_size);
-// 	close(fd);
-
-// 	PCRE2_SPTR strCompare =(PCRE2_SPTR) fileBuffer;
-// 	PCRE2_SPTR pattern =(PCRE2_SPTR) myArgs.T; // remplacer "/nd/" par myArgs.T
-
-// 	pcre2_code* regexPattern;
-// 	pcre2_match_data *match_data;
-// 	int errornumber;
-// 	PCRE2_SIZE erroroffset;
-
-// 	regexPattern = pcre2_compile(pattern,PCRE2_ZERO_TERMINATED,0,&errornumber,&erroroffset,NULL); //On compile le pattern
-
-
-// 	//on regarde si la compilation a marchée
-// 	if (regexPattern == NULL){
-//  		PCRE2_UCHAR buffer[256];
-//   		pcre2_get_error_message(errornumber, buffer, sizeof(buffer));
-//   		printf("PCRE2 compilation failed at offset %d: %s\n", (int)erroroffset,buffer);
-//   		return 0;
-//   	} else {
-
-// 		match_data = pcre2_match_data_create_from_pattern(regexPattern, NULL);
-
-// 		// On regarde dans le fichier si quelque chose correspond au pattern
-// 		int a = pcre2_match(regexPattern,strCompare,strlen((char *)strCompare),0,0,match_data,NULL);
-
-// 	  	pcre2_match_data_free(match_data);   /* Release memory used for the match */
-// 	  	pcre2_code_free(regexPattern);                 /* data and the compiled pattern. */
-	  	
-// 		if(a>=1){
-// 			return 1;
-// 		} else {
-// 			return 0;
-// 		}		
-// 	}
-// 	return 0;
-// }
 
 bool hasName(char* name){
 
